52deb2bba1a07b5f8ce28424bfa87718110d9f70
mva <mvabangan@gmail.com>
2026-03-04 19:10:40 +0800

    Challenge 3: The Tabbed Skills Interface

--- Portfolio_CIS1202/script.js
+++ Portfolio_CIS1202/script.js
@@ -1,3 +1,16 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
+
+function showSkills(type) {
+    const tech = document.getElementById('tech-skills');
+    const soft = document.getElementById('soft-skills');
+
+    if (type === 'tech') {
+        tech.classList.remove('hidden');
+        soft.classList.add('hidden');
+    } else {
+        soft.classList.remove('hidden');
+        tech.classList.add('hidden');
+    }
+}
